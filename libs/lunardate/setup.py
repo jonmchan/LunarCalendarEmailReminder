8dc00f594993944b441f936fd567f5009d3f1bf9
Jonathan Chan <jc@jmccc.com>
2013-11-14 18:21:05 -0500

    added lunardate lib

---
+++ libs/lunardate/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+import lunardate
+
+setup(name='lunardate',
+      version='0.1.4',
+      py_modules = ['lunardate'],
+      description = 'A Chinese Calendar Library in Pure Python',
+      long_description = lunardate.__doc__,
+      author = 'LI Daobing',
+      author_email = 'lidaobing@gmail.com',
+      url = 'http://code.google.com/p/python-lunardate',
+      license = 'GPLv3',
+      classifiers = [
+                     'Development Status :: 4 - Beta',
+                     'Programming Language :: Python',
+                     'License :: OSI Approved :: GNU General Public License (GPL)',
+                     'Operating System :: OS Independent',
+                     'Topic :: Software Development :: Libraries :: Python Modules'
+                     ]
+      )
