0a74f90390a1e4e85461338127e6181bc776cf79
Jonathan Chan <jc@jmccc.com>
2013-11-15 01:48:39 -0500

    added lunar date and western date functionality, added daily and user reminder limits

--- lunarcalendarportal.py
+++ lunarcalendarportal.py
@@ -11,83 +11,14 @@ from messagingjob import MessagingJob
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
-MAIN_PAGE_FOOTER_TEMPLATE = """\
-    <form action="/sign" method="post">
-      <div><label for="content">Reminder: </label><input name="content"
-      type="text" /></div>
-      <div>
-      <label for="year">Year: </label>
-      <select name="year">
-      <option value="1900">1900</option>
-      <option value="1901">1901</option>
-      </select>
-      
-      <label for="month">Month: </label>
-      <select name="month">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13 (lunar leap month)</option>
-      </select>
-
-      <label for="day">Day: </label>
-      <select name="day">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-      <option value="7">7</option>
-      <option value="8">8</option>
-      <option value="9">9</option>
-      <option value="10">10</option>
-      <option value="11">11</option>
-      <option value="12">12</option>
-      <option value="13">13</option>
-      <option value="14">14</option>
-      <option value="15">15</option>
-      <option value="16">16</option>
-      <option value="17">17</option>
-      <option value="18">18</option>
-      <option value="19">19</option>
-      <option value="20">20</option>
-      <option value="21">21</option>
-      <option value="22">22</option>
-      <option value="23">23</option>
-      <option value="24">24</option>
-      <option value="25">25</option>
-      <option value="26">26</option>
-      <option value="27">27</option>
-      <option value="28">28</option>
-      <option value="29">29</option>
-      <option value="30">30</option>
-      <option value="31">31</option>
-      </select>
-      </div>
-      <div><input type="submit" value="Add Email Reminder"></div>
-    </form>
-
-    <hr>
-
-    <a href="%s">%s</a>
-
-  </body>
-</html>
-"""
-
-
 
 DEFAULT_MESSAGING_JOB_NAME = 'default_job_queue'
+
+## prevent flooding
+MAX_ENTRIES_PER_USER=5
+
+## Mostly set to prevent hitting Google AppEngine Email limits
+MAX_ENTRIES_PER_DAY=8
 
 JINJA_ENVIRONMENT = jinja2.Environment(
         loader=jinja2.FileSystemLoader(os.path.dirname(__file__)+"/templates"),
@@ -106,33 +37,20 @@ class MainPage(webapp2.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
-        #self.response.write('<html><body>')
-
         messaging_jobs_query = MessagingJob.query(
             ancestor=messagingJob_key(DEFAULT_MESSAGING_JOB_NAME))
         messaging_jobs_query = messaging_jobs_query.filter(
             MessagingJob.owner == user).order(-MessagingJob.created_date)
         messaging_jobs = messaging_jobs_query.fetch(10)
 
-        #for job in messaging_jobs:
-        #    self.response.write(
-        #            '<b>%s</b> wrote: <a href="/delete?id=%s">Delete</a>' %
-        #            (job.date,job.key.urlsafe()))
-        #    self.response.write('<blockquote>%s</blockquote>' %
-        #                        cgi.escape(job.note))
-            
         logout_url = users.create_logout_url(self.request.uri)
         logout_url_linktext = 'Logout'
-
-        # Write the submission form and the footer of the page
-        #sign_query_params = urllib.urlencode({'guestbook_name': DEFAULT_MESSAGING_JOB_NAME })
-        #self.response.write(MAIN_PAGE_FOOTER_TEMPLATE %
-        #                    (sign_query_params, url, url_linktext))
 
         template_values = {
                 'jobs': messaging_jobs,
                 'logout_url': logout_url,
                 'logout_url_linktext': logout_url_linktext,
+                'error': self.request.get('msg',''),
                 }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -149,7 +67,7 @@ class DeleteJob(webapp2.RequestHandler):
         
         msgJobKey=ndb.Key(urlsafe=self.request.get('id'))
         msgJobKey.delete()
-        self.redirect('/')
+        self.redirect('/?msg=success_deleted')
 
 
 
@@ -170,16 +88,38 @@ class AddJob(webapp2.RequestHandler):
 
         msg_job.note = self.request.get('content')
 
-        msg_job.date = date(int(self.request.get('year')),
-                int(self.request.get('month')), int(self.request.get('day')))
+        if self.request.get('date_type') == 'lunar':
+            try:
+                msg_job.date = LunarDate(int(self.request.get('year')),
+                        int(self.request.get('month')), int(self.request.get('day'))).toSolarDate()
+            except ValueError:
+                self.redirect('/?msg=invalid_date')
+                return
+        else:
+            try:
+                msg_job.date = date(int(self.request.get('year')),
+                        int(self.request.get('month')), int(self.request.get('day')))
+            except ValueError:
+                self.redirect('/?msg=invalid_date')
+                return
+
+        base_messaging_jobs_query = MessagingJob.query(
+            ancestor=messagingJob_key(DEFAULT_MESSAGING_JOB_NAME))
+        messaging_jobs_query_by_user = base_messaging_jobs_query.filter(
+            MessagingJob.owner == user)
+        if messaging_jobs_query_by_user.count() >= MAX_ENTRIES_PER_USER:
+            self.redirect('/?msg=exceed_user_quota')
+            return
+
+        messaging_jobs_query_by_date = base_messaging_jobs_query.filter(
+                MessagingJob.date == msg_job.date)
+        if messaging_jobs_query_by_date.count() >= MAX_ENTRIES_PER_DAY:
+            self.redirect('/?msg=exceed_daily_quota')
+            return
+
         msg_job.put()
 
         self.redirect('/')
-        
-
-        #self.response.write('<html><body>'+user.email()+' wrote:<pre>')
-        #self.response.write(cgi.escape(self.request.get('content')))
-        #self.response.write('</pre></body></html>')
 
 
 application = webapp2.WSGIApplication([
--- messagingjob.py
+++ messagingjob.py
@@ -1,9 +1,32 @@
 from google.appengine.ext import ndb
+from lunardate import LunarDate
+import datetime
+
 
 class MessagingJob(ndb.Model):
     owner = ndb.UserProperty()
     date = ndb.DateProperty()
     note = ndb.StringProperty(indexed=False)
     created_date = ndb.DateProperty(auto_now_add=True)
+
+    def getLunarDate(self):
+        ld = LunarDate.fromSolarDate(self.date.year, self.date.month,self.date.day)
+        return ld
+
+    def getFormattedLunarDate(self):
+        ld = self.getLunarDate()
+        return str(ld.year) + "/"+ str(ld.month)+"/"+str(ld.day)
+
+    def getNextRun(self):
+        try:
+            ld = self.getLunarDate()
+            if datetime.date.today() < LunarDate(datetime.date.today().year,ld.month,ld.day).toSolarDate():
+                ld = LunarDate(datetime.date.today().year,ld.month,ld.day)
+            else:
+                ld= LunarDate(datetime.date.today().year+1,ld.month,ld.day)
+            return ld.toSolarDate().strftime('%m/%d/%Y')
+        except ValueError:
+            return "Date before 1900 or after 2050 Error"
+
 
 
